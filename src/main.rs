8c04ab8cf4c8ce4a448d55ca97dbd8053652837a
sipasi <tanksasha77@gmail.com>
2025-09-18 22:50:15 +0300

    chore: update main

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,24 @@ mod core;
 
 use core::shapes::point::Point;
 
-use crate::core::plot::plot_builder::PlotBuilder;
+use crate::core::plot::plotly_builder::PlotlyBuilder;
+use crate::core::plot::plotters_builder::PlottersBuilder;
 use crate::core::shapes::point_generator::PointGenerator;
 use crate::core::shapes::triangle::Triangle;
 use crate::core::triangulation::bowyer_watson_triangulator::BowyerWatsonTriangulator;
 
 fn main() {
-    let points: Vec<Point> = PointGenerator::square(5);
+    let point_count: usize = 3;
+
+    let points: Vec<Point> = PointGenerator::square(point_count); // FPointGenerator::heart(1f64)
 
     let triangles: Vec<Triangle> = BowyerWatsonTriangulator::compute(&points, false);
 
-    let _ = PlotBuilder::triangles(&triangles);
+    // let _ = PlottersBuilder::triangles(&triangles);
+    let _ = PlotlyBuilder::triangles(&triangles);
 
-    for triangle in triangles {
-        println!("{:?}", triangle);
-    }
+    println!(
+        "point count: {point_count} make triangles: {:?}",
+        triangles.len()
+    );
 }
