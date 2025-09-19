77b34d047dad1b20f0d69d47b2f6d5c88a779d1b
sipasi <tanksasha77@gmail.com>
2025-09-19 21:35:23 +0300

    chore: update main

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,19 @@ use crate::core::plot::plotly_builder::PlotlyBuilder;
 use crate::core::shapes::point_generator::PointGenerator;
 use crate::core::shapes::triangle::Triangle;
 use crate::core::triangulation::bowyer_watson_triangulator::BowyerWatsonTriangulator;
+use crate::core::triangulation::circumable::Circumable;
 
 fn main() {
-    let point_count: usize = 5;
+    let point_count: usize = 100;
 
     let points: Vec<Point> = PointGenerator::random(point_count);
 
     let triangles: Vec<Triangle> = BowyerWatsonTriangulator::compute(&points, false);
 
-    let show_circumcircle = true;
-    let _ = PlotlyBuilder::triangles(&triangles, show_circumcircle);
+    let show_circumcircle = false;
+    let skip_big_circumcircle = true;
+    let _ = PlotlyBuilder::triangles(&triangles, show_circumcircle, skip_big_circumcircle);
 
-    println!("point count: {point_count}");
+    println!("point count: {:?}", points.len());
     println!("triangles: {:?}", triangles.len());
 }
